2ca9335b7d879c7c1b729b345e96c42592fca278
Burstniart <v4gr33n@hotmail.com>
2022-04-28 14:11:11 -0500

    Add code for static method readJsonFIle in Reader class / add testRun.json file for testing

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,17 +1,10 @@
 const Reader = require("./../../lib/utils/Reader")
 describe("Test suit for Reader", () => {
-    test("1) PLaceholder", () => {
-        const explorers = [{"id": "6264d5d89f1df827eb84bb23","name": "Warren","email": "Todd@visualpartnership.xyz",
-            "credits": 508,"enrollments": ["Visual Thinking Intermedio","Visual Thinking Avanzado"],
-            "previousCourses": 1,"haveCertification": true},
-          {"id": "6264d5d85cf81c496446b67f","name": "Lucinda","email": "Sexton@visualpartnership.xyz",
-            "credits": 677,"enrollments": ["Visual Thinking Avanzado"],"previousCourses": 6,
-            "haveCertification": true
-          }];
-          const iRead = Reader.readJson("explorers");
+    test("1) Test readJsonFile static method", () => {
+        const iRead = Reader.readJsonFile('testRun.json');
 
-        expect(explorers[0].name).toBe("Warren");
-        expect(explorers[1].credits).toBe(677);
+        expect(iRead[0].name).toBe("Warren");
+        expect(iRead[1].credits).toBe(677);
 
     });
 });
