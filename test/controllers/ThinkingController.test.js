619553e455bc074201efea4fdfe15ce991be12fb
Burstniart <v4gr33n@hotmail.com>
2022-04-28 22:29:48 -0500

    Add code for getStudents method in ThinkingController method

--- test/controllers/ThinkingController.test.js
+++ test/controllers/ThinkingController.test.js
@@ -3,7 +3,7 @@ const ThinkingController = require("./../../lib/controllers/ThinkingController")
 describe("Test suit for Thinking Controller", () => {
     test("1) Testting getStudents method", () => {
         const thinkers = ThinkingController.getStudents();
-
+        
         expect(thinkers[0].name).toBe("Warren");
     });
 });
