ed86a6d9b5b690d132d6c9bf9829470ec71f1c8b
Burstniart <v4gr33n@hotmail.com>
2022-04-28 22:35:11 -0500

    Add test for getCertified method in ThinkingController

--- test/controllers/ThinkingController.test.js
+++ test/controllers/ThinkingController.test.js
@@ -1,9 +1,14 @@
 const ThinkingController = require("./../../lib/controllers/ThinkingController");
 
 describe("Test suit for Thinking Controller", () => {
-    test("1) Testting getStudents method", () => {
+    test("1) Testing getStudents method", () => {
         const thinkers = ThinkingController.getStudents();
-        
+
         expect(thinkers[0].name).toBe("Warren");
     });
+    test("2) Testting getCertified method", () => {
+        const certifieds = ThinkingController.getCertified();
+        console.log(certifieds);
+        expect(certifieds[0]).toBe("Todd@visualpartnership.xyz");
+    })
 });
